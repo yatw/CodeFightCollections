22fde2b7caaa38f69b9cf56e0fd4e0d99c189b37
yatw <yatw@uci.edu>
2018-05-18 11:17:06 -0700

    stored given tasks

--- roadmap/main.cpp
+++ roadmap/main.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <stdio.h>  //for printf
 using namespace std;
 
+struct pinfo{
+
+    vector<string> workers;
+    string startday, dueday;
+};
+
+map <string, pinfo> projects;
 
 vector<vector<string>> roadmap(vector<vector<string>> tasks, vector<vector<string>> queries) {
 
     vector<vector<string>> result;
 
-    for (int i = 0; i < queries.size(); i++){
-        vector<string> working_on = {};
-        result.insert(working_on);
+
+    for (int i = 0; i < tasks.size(); i++){
+
+        vector<string> task = tasks[i];
+
+        pinfo p;
+        p.startday = task[1];
+        p.dueday = task[2];
+
+
+
+         // loop through the list of workers
+        for (int ii = 3; ii < task.size(); ii++){
+            p.workers.push_back(task[ii]);
+        }
+
+        projects.insert(make_pair(task[0], p));
 
     }
+/***
+    for (auto p : projects){
+        cout << p.first << endl;
+        cout << p.second.startday << endl;
+        cout << p.second.dueday << endl;
+        for (auto w : p.second.workers){
+            cout << w << endl;
+        }
+
+
+    }
+    ***/
     return result;
 }
 
