54fa9594d7d08a4998cc113710c058db361dcbc3
yatw <yatw@uci.edu>
2018-05-19 11:21:14 -0700

    sort project order using priority queue

--- roadmap/main.cpp
+++ roadmap/main.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdio.h>  //for printf
 #include <algorithm> //for find
+#include <queue>          // std::priority_queue
+#include <functional>
 using namespace std;
 
 struct pinfo{
@@ -12,7 +14,58 @@ struct pinfo{
     string startday, dueday;
 };
 
-map <string, pinfo> projects;
+struct Compare
+{
+
+    map <string, pinfo> p;
+
+    Compare( map <string, pinfo>*  projects){
+        p = *projects;
+    }
+
+    bool operator() (string date1, string date2)
+    {
+        // compare two date, return true if lhs < rhs
+
+        string d1 = p[date1].dueday;
+        string d2 = p[date2].dueday;
+
+        // convert string to char*
+        vector<char> v1(d1.length() + 1);
+        strcpy(&v1[0], d1.c_str());
+        char* date1_s = &v1[0];
+
+        int date1_y = atoi(strtok(date1_s, "-"));
+        int date1_m = atoi(strtok(NULL, "-"));
+        int date1_d = atoi(strtok(NULL, "-"));
+
+        vector<char> v2(d2.length() + 1);
+        strcpy(&v2[0], d2.c_str());
+        char* date2_s = &v2[0];
+
+        int date2_y = atoi(strtok(date2_s, "-"));
+        int date2_m = atoi(strtok(NULL, "-"));
+        int date2_d = atoi(strtok(NULL, "-"));
+
+        if (date1_y > date1_y){
+            return true;
+        }else if (date1_y == date2_y){
+
+            if (date1_m > date2_m){
+                return true;
+            }else if (date1_m == date2_m){
+
+                if (date1_d > date2_d || date1_d == date2_d){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+
 
 bool in_between(string startday, string dueday, string date){
     // "2017-02-01","2017-03-01"
@@ -85,48 +138,68 @@ bool in_between(string startday, string dueday, string date){
 
 vector<vector<string>> roadmap(vector<vector<string>> tasks, vector<vector<string>> queries) {
 
+
+    map <string, pinfo> projects;
+    map <string, vector<string>> workingon;
+
     vector<vector<string>> result;
 
-    // store all the task into a project datastructure
+    // store all the task into a project data structure
     for (int i = 0; i < tasks.size(); i++){
 
         vector<string> task = tasks[i];
+        string task_name = task[0];
 
         pinfo p;
         p.startday = task[1];
         p.dueday = task[2];
          // loop through the list of workers
         for (int ii = 3; ii < task.size(); ii++){
-            p.workers.push_back(task[ii]);
+
+            string worker_name = task[ii];
+            p.workers.push_back(worker_name);
+
+            // also keep another map keep tracks of the project they working on
+            if (workingon.find(worker_name) != workingon.end()){
+                workingon[worker_name].push_back(task_name);
+            }else{
+                vector<string> t;
+                t.push_back(task_name);
+                workingon.insert(make_pair(worker_name,t));
+            }
+
         }
-        projects.insert(make_pair(task[0],p));
+        projects.insert(make_pair(task_name,p));
     }
 
 
 
     // find result from the data structure
-    for (int i = 0; i < queries.size(); i++){ // each query is one worker
-
-        vector<string> w; // the result of 1 worker
-
+    for (int i = 0; i < queries.size(); i++){
+            // each query is one worker
         vector<string> query = queries[i];
-
         string worker = query[0];
         string date = query[1];
 
-        // go through each project, and find if that worker is working on it
-        for (auto p: projects){
+        priority_queue<string, std::vector<string>, Compare> q(&projects); // store all the projects in order
 
+        // loop through all the projects this worker working on
+        for (auto p :workingon[worker]){
+            // p is just the name of each project
 
-            if(find(p.second.workers.begin(), p.second.workers.end(), worker) != p.second.workers.end()) {
-
-            // if that worker is associated with this project
-            // check if the date is in between the startday and dueday
-                if (in_between(p.second.startday, p.second.dueday, date)){
-                    w.push_back(p.first);
-                }
+            if (in_between(projects[p].startday, projects[p].dueday, date)){
+                q.push(p);
             }
         }
+
+        // after getting all the projects in order, put them in a vector
+        vector<string> w;
+        while (!q.empty()) {
+          w.push_back(q.top());
+          q.pop();
+        }
+
+
 
         result.push_back(w);
     }
@@ -138,27 +211,31 @@ vector<vector<string>> roadmap(vector<vector<string>> tasks, vector<vector<strin
 
 int main()
 {
-    vector<vector<string>> tasks = {{"A","2017-02-01","2017-03-01","Sam","Evan","Troy"}, {"B","2017-01-16","2017-02-15","Troy"}, {"C","2017-02-13","2017-03-13","Sam","Evan"}};
-    vector<vector<string>> queries = {{"Evan","2017-03-10"},{"Troy","2017-02-04"}};
+    vector<vector<string>> tasks = {{"LNWBN","2017-08-13","2017-12-24","Corey","Kyle","Kaleb","Reuben"}, {"NSXEN","2017-08-20","2017-09-18","Kai"}, {"DNMDC","2017-06-19","2017-08-07","Kaleb","Kai","Kyle","Reuben"}, {"UYWEQ","2017-04-23","2017-07-18","Corey","Kyle","Reuben","Kai"}, {"LIVNH","2017-11-01","2017-12-24","Kaleb","Kai"}};
+    vector<vector<string>> queries = {{"Corey","2017-10-21"},{"Reuben","2017-03-16"},{"Kaleb","2017-11-22"},{"Kaleb","2017-03-22"},{"Reuben","2017-10-06"}};
     vector<vector<string>> result = roadmap(tasks, queries);
 
-    for (auto r: result){
-        for (auto re: r){
-            cout << re << endl;
+    for (auto e : result){
+        for (auto r: e){
+            cout << r << endl;
         }
-        cout << "------------------" << endl;
+        cout << "------------" << endl;
     }
+
+
     return 0;
 }
 
 
 /***
-tasks: [["A","2017-02-01","2017-03-01","Sam","Evan","Troy"],
- ["B","2017-01-16","2017-02-15","Troy"],
- ["C","2017-02-13","2017-03-13","Sam","Evan"]]
-queries: [["Evan","2017-03-10"],
- ["Troy","2017-02-04"]]
-
- [["C"],
- ["B","A"]]
+tasks: [["LNWBN","2017-08-13","2017-12-24","Corey","Kyle","Kaleb","Reuben"],
+ ["NSXEN","2017-08-20","2017-09-18","Kai"],
+ ["DNMDC","2017-06-19","2017-08-07","Kaleb","Kai","Kyle","Reuben"],
+ ["UYWEQ","2017-04-23","2017-07-18","Corey","Kyle","Reuben","Kai"],
+ ["LIVNH","2017-11-01","2017-12-24","Kaleb","Kai"]]
+queries: [["Corey","2017-10-21"],
+ ["Reuben","2017-03-16"],
+ ["Kaleb","2017-11-22"],
+ ["Kaleb","2017-03-22"],
+ ["Reuben","2017-10-06"]]
  ***/
